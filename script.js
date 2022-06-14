b281c1187a952d31bdf52ccbffd1fece09ea833c
sahrarafaela <sararafaela704@gmail.com>
2022-06-14 15:54:50 -0300

    java

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let refrigerante = Number (prompt ("Qual o valor do refrigerante?"))
+let macarrao = Number (prompt ("Qual o valor do macarrao?"))
+let ervilha = Number (prompt ("Qual o valor da ervilha?"))
+let arroz = Number (prompt ("Qual o valor do arroz?"))
+let feijao = Number (prompt ("Qual o valor do feijao?"))
+const vinho = 70.00
+let totalCompra = (refrigerante*3) + (macarrao*4) + (ervilha*3) + (arroz*3) + (feijao*2)
+let totalDividido = (totalCompra + (vinho*3))/ 2
+let total = (alert(`O valor total da compra é  ${totalCompra}`))
+
+if (totalCompra%2 == 0){
+   alert(`Deu par!! Eu vou pagar: ${totalCompra/2} e você: ${totalCompra/2+vinho}`)
+} else { 
+    alert("Deu ímpar!! Você vai pagar o valor do vinho")
+}
